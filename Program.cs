3be97d4351fbf44269a88f6fb046c33e8cb89360
lucky jay <2802571486@qq.com>
2023-06-06 09:28:07 +0800

    BtnExt

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace UControls
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            // Application.Run();
         }
     }
 }
---
+++ UControl/Button/UCBtnExt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UControls.UControl.Button
+{
+    /// <summary>
+    /// Class UCBtnExt.
+    /// Implements the <see cref="UControlBase"/>
+    /// </summary>
+    /// <seealso cref="UControlBase"/>
+    [DefaultEvent("BtnClick")]
+    public partial class UCBtnExt : UControlBase
+    {
+        #region 字段属性
+        private bool enableMouseEffect = false;
+        [Description("是否启用鼠标效果"), Category("自定义")]
+        public bool EnableMouseEffect
+        {
+            get { return this.enableMouseEffect; }
+            set { enableMouseEffect = value; }
+        }
+        #endregion
+
+        public UCBtnExt()
+        {
+            InitializeComponent();
+        }
+    }
+}
